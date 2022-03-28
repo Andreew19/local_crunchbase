bc57413d4474c5dc142ed4b24437e8887ffa533b
Andreew19 <58325735+Andreew19@users.noreply.github.com>
2022-03-28 13:33:23 +0400

    add multysellect

--- startups/models.py
+++ startups/models.py
@@ -9,12 +9,6 @@ class Categories(models.Model):
   def __str__(self):
         return self.name
 
-MY_LIST = (
-    ('', 'none'),
-    (1, Categories.objects.get(id=1)),
-    (2,Categories.objects.get(id=2)),
-    (3,Categories.objects.get(id=3))
- )
 
 class Articles(models.Model):
     title = models.CharField(max_length=256)
@@ -23,7 +17,7 @@ class Articles(models.Model):
     publicated_at = models.DateTimeField('date published')
     creator = models.CharField(max_length=256)
     type = models.CharField(max_length=256, blank=True, null=True)
-    article_categories = models.ManyToManyField(Categories, choices=MY_LIST)
+    article_categories = models.ManyToManyField(Categories)
 
 
 
