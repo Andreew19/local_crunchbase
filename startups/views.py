a6a9a737095572271626433047d2c69a36e45ac6
Andreew19 <58325735+Andreew19@users.noreply.github.com>
2022-04-03 00:34:22 +0400

    api v1 test

--- startups/views.py
+++ startups/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .serializers import CategoriesSerializing
+from .models import Categories
 # Create your views here.
+
+class CategoriesViewSet(viewsets.ModelViewSet):
+  queryset = Categories.objects.all()
+  serializer_class = CategoriesSerializing
