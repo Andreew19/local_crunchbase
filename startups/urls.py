9a04d50fa7915576e054f47f18b4087399e56ec1
Andreew19 <58325735+Andreew19@users.noreply.github.com>
2022-04-05 20:27:53 +0400

    add all tables in api

--- startups/urls.py
+++ startups/urls.py
@@ -1,9 +1,13 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import CategoriesViewSet
+from .views import CategoriesViewSet, ArticlesViewSet,StartupsViewSet,TagGroupsViewSet,TagsViewSet
 
 router = routers.DefaultRouter()
-router.register(r'category', CategoriesViewSet)
+router.register(r'categories', CategoriesViewSet)
+router.register(r'articles', ArticlesViewSet)
+router.register(r'startups', StartupsViewSet)
+router.register(r'tags_groups', TagGroupsViewSet)
+router.register(r'tags', TagsViewSet)
 
 urlpatterns = [
   path('', include(router.urls)),
