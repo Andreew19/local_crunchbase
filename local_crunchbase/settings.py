a9eaa8c829fcf601e09804519cc4a437fcb826f6
Stanislav Chereshkevich <stanislav.chereshkevich@buyk.com>
2022-08-04 13:10:33 +0300

    Fix cors

--- local_crunchbase/settings.py
+++ local_crunchbase/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'rest_framework',
     'startups.apps.StartupsConfig',
     'django.contrib.admin',
@@ -43,10 +44,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -130,3 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 GRAPHENE = {
     "SCHEMA": "startups.schema.schema"
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8080',
+]
